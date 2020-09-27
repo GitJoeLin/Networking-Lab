12a58bb833667b45b081344cb41675136ee2311d
AndrewShim0428 <34689452+AndrewShim0428@users.noreply.github.com>
2020-09-28 02:00:00 +0900

    Update ClientServerHandler.java

--- ClientServerHandler.java
+++ ClientServerHandler.java
@@ -15,13 +15,36 @@ public class ClientServerHandler implements Runnable {
     public void run() {
         try {
             String incoming = "";
+            String userName = "";
 
             while( (incoming = socketIn.readLine()) != null) {
                 //handle different headers
                 //WELCOME
                 //CHAT
                 //EXIT
-                System.out.println(incoming);
+                if(incoming.startsWith("SUBMITNAME")){
+                    System.out.println("Enter your username: ");
+                }
+                else if(incoming.startsWith("WELCOME")){
+                    System.out.println(incoming.substring(8) + " has joined.");
+                    userName = incoming.substring(8);
+                }
+                else if(incoming.startsWith("CHAT")){
+                    int posname = incoming.indexOf(":");
+                    if(!(incoming.substring(5, posname).equals(userName))){
+                        System.out.println(incoming.substring(5));
+                    }
+                }
+                else if(incoming.startsWith("PCHAT"+userName)){
+                    int posname = incoming.indexOf(":");
+                    if(!(incoming.substring(5, posname).equals(userName))){
+                        System.out.println(incoming.substring(6+userName.length()));
+                    }
+                }
+                else if(incoming.startsWith("EXIT")){
+                    int posname = incoming.indexOf(" ");
+                    System.out.println(incoming.substring(posname+1) + " has left.");
+                }
             }
         } catch (Exception ex) {
             System.out.println("Exception caught in listener - " + ex);
