6684cc7d16f8631c4bcec8e7d80fcbc73ee8faf2
AndrewShim0428 <34689452+AndrewShim0428@users.noreply.github.com>
2020-09-29 13:52:27 +0900

    Update ClientServerHandler.java

--- ClientServerHandler.java
+++ ClientServerHandler.java
@@ -1,4 +1,4 @@
-package NetworkingLab;
+package day5_bca;
 
 import java.io.BufferedReader;
 import java.net.Socket;
@@ -6,6 +6,7 @@ import java.nio.Buffer;
 
 public class ClientServerHandler implements Runnable {
     BufferedReader socketIn;
+    int cookie = 0;
 
     public ClientServerHandler(BufferedReader socketIn) {
         this.socketIn = socketIn;
@@ -37,13 +38,29 @@ public class ClientServerHandler implements Runnable {
                 }
                 else if(incoming.startsWith("PCHAT"+userName)){
                     int posname = incoming.indexOf(":");
-                    if(!(incoming.substring(5, posname).equals(userName))){
+                    if(!(incoming.substring(6, posname).equals(userName))){
                         System.out.println(incoming.substring(6+userName.length()));
+                    }
+                }
+                else if(incoming.startsWith("COOKIE"+userName)){
+                    int posname = incoming.indexOf(" ");
+                    if(!(incoming.substring(7, posname).equals(userName))){
+                        System.out.println(incoming.substring(7+userName.length()));
+                        cookie++;
+                        if(cookie<=1) {
+                            System.out.println("You have " + cookie + " cookie!");
+                        }
+                        else{
+                            System.out.println("You have " + cookie + " cookies!");
+                        }
                     }
                 }
                 else if(incoming.startsWith("EXIT")){
                     int posname = incoming.indexOf(" ");
                     System.out.println(incoming.substring(posname+1) + " has left.");
+                }
+                else{
+                    System.out.println(incoming);
                 }
             }
         } catch (Exception ex) {
