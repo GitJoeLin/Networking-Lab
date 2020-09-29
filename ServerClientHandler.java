b0048ab1085afdcc1e295bcfebb69c1a5e98ed0b
AndrewShim0428 <34689452+AndrewShim0428@users.noreply.github.com>
2020-09-29 13:52:41 +0900

    Update ServerClientHandler.java

--- ServerClientHandler.java
+++ ServerClientHandler.java
@@ -1,20 +1,32 @@
-package NetworkingLab;
+package day5_bca;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Timer;
 
 public class ServerClientHandler implements Runnable{
     // Maintain data about the client serviced by this thread
-    private ClientConnectionData client;
-    private final ArrayList<ClientConnectionData> clientList;
-    private ArrayList<String> userNames = new ArrayList<>();
+    ClientConnectionData client;
+    final ArrayList<ClientConnectionData> clientList;
+    ArrayList<String> userNames = new ArrayList<>();
+    boolean cookie;
+    final List<String> cookieWords = Arrays.asList("plough", "passenger", "basketball", "instrument", "competition", "knowledge", "quicksand", "amusements"
+            , "observation", "crocodile", "hippopotamus", "librarian", "maintenance", "appointment", "establishment", "psychology"
+            , "enthusiasm", "chinchilla", "dormouse", "basilisk", "orangutan", "wolverine", "platypus", "armadillo", "reindeer"
+            , "hamburger", "macaroni", "spearmint", "oregano", "mushroom", "marshmallow", "strawberry", "potato", "spaghetti", "honeydew"
+            , "cantaloupe", "grapefruit", "watermelon", "sandwich", "helicopter", "houseplant", "aquarium", "magnetism", "emergency");
+    String currentWord;
+    Timer timer = new Timer();
 
     public ServerClientHandler(ClientConnectionData client, ArrayList<ClientConnectionData> clientList){
         this.client = client;
         this.clientList = clientList;
+        cookie = false;
     }
 
     public void broadcast(String msg){
@@ -28,6 +40,13 @@ public class ServerClientHandler implements Runnable{
         } catch(Exception ex){
             System.out.println("broadcast caught exception: " + ex);
             ex.printStackTrace();
+        }
+    }
+    public void cookieGiver(){
+        if(!cookie){
+            currentWord = cookieWords.get((int)(Math.random()*(cookieWords.size())));
+            broadcast("Whoever types "+ currentWord + " first gets a cookie!");
+            cookie = true;
         }
     }
     public boolean validName(String name){
@@ -52,7 +71,7 @@ public class ServerClientHandler implements Runnable{
                 else if (userName.startsWith("* ")){
                     userName = userName.substring(2);
                     synchronized (clientList){
-                        if(userNames.size()!= 0) {
+                        if(userNames.size()!=0) {
                             if (!userNames.contains(userName) && validName(userName)) {
                                 client.setUserName(userName);
                                 clientList.add(client);
@@ -77,7 +96,7 @@ public class ServerClientHandler implements Runnable{
 
 
             String incoming = "";
-
+            cookieGiver();
             while ( (incoming = in.readLine()) != null) {
                 // handle messages
                 if(incoming.startsWith("* ")){
@@ -91,16 +110,29 @@ public class ServerClientHandler implements Runnable{
                 else if(incoming.startsWith("@")){
                     if(incoming.contains("* ")) {
                         int namePrivate = incoming.indexOf("*");
-                        String name = incoming.substring(1, namePrivate - 1);
-                        String chat = incoming.substring(namePrivate + 2).trim();
+                        String name = incoming.substring(1, namePrivate-1);
+                        String chat = incoming.substring(namePrivate+2).trim();
                         if (userNames.contains(name)) {
                             String msg = String.format("PCHAT%s %s (private): %s", name, client.getUserName(), chat);
                             broadcast(msg);
                         }
                     }
-
                 }
-
+                else if(incoming.startsWith("#")){
+                    if(currentWord.equals(incoming.substring(2))){
+                        String msg = String.format("COOKIE%s You get a cookie!", client.getUserName());
+                        broadcast(msg);
+                        int current = client.getCookies();
+                        current++;
+                        client.setCookies(current);
+                        if(current <=1 ){
+                            broadcast(client.getUserName() + " has " + client.getCookies() + " cookie!");
+                        }
+                        broadcast(client.getUserName() + " has " + client.getCookies() + " cookies!");
+                        cookie = false;
+                        cookieGiver();
+                    }
+                }
                 else if(incoming.startsWith("QUIT")){
                     break;
                 }
